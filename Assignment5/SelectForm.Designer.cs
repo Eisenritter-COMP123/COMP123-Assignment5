de9871d1bec946c4c0e61d425b6f38cb3bfabc6a
George Zhou <georgezhou89@hotmail.com>
2019-07-31 08:55:07 -0400

    Select From Updated

--- Assignment5/SelectForm.Designer.cs
+++ Assignment5/SelectForm.Designer.cs
@@ -29,24 +29,39 @@
         private void InitializeComponent()
         {
             this.SelectionLabel = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // SelectionLabel
             // 
+            this.SelectionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.SelectionLabel.Location = new System.Drawing.Point(0, 0);
             this.SelectionLabel.Name = "SelectionLabel";
             this.SelectionLabel.Size = new System.Drawing.Size(801, 53);
             this.SelectionLabel.TabIndex = 0;
-            this.SelectionLabel.Text = "Placeholder";
+            this.SelectionLabel.Text = "DollarComputer Hardware List";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(8, 56);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(1878, 834);
+            this.dataGridView1.TabIndex = 1;
             // 
             // SelectForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1898, 1024);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.SelectionLabel);
             this.Name = "SelectForm";
             this.Text = "Select Form";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -54,5 +69,6 @@
         #endregion
 
         private System.Windows.Forms.Label SelectionLabel;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
